903cf00e65444f892f17edb7b2eaca2dbd03e222
Shyedelta <shirodelta@gmail.com>
2024-02-07 00:52:51 +0100

    Actualizacion de home

--- src/componentes/MovieList.jsx
+++ src/componentes/MovieList.jsx
@@ -1,18 +1,21 @@
 import Card from "./Card"
 import React, { useEffect, useState, useRef } from 'react';
 import "../styles/stylesMovieList.css";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import { fetchMovies } from "../funciones/fetch"
 import { Link } from 'react-router-dom';
-
-import { animate, motion, useMotionValue, useScroll } from "framer-motion";
-
+import { animate, motion, useMotionValue } from "framer-motion";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
 
-  const carruselRef = useRef(null);
+  //HOOKS DE CARRUSEL
+  const ref = useRef(null);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [trackMouse, setTrackMouse] = useState(false);
+  const [animationComplete, setAnimationComplete] = useState(true);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,42 +29,19 @@ const MovieList = () => {
     fetchData();
   }, []);
 
-  const moverIzquierda = () => {
-    if (carruselRef.current) {
-      carruselRef.current.scrollLeft -= 200;
-    }
-  };
-
-  const moverDerecha = () => {
-    if (carruselRef.current) {
-      carruselRef.current.scrollLeft += 200;
-    }
-  };
-
-  const ref = useRef(null);
-  const { scrollXProgress } = useScroll({ container: ref });
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-  const [trackMouse, setTrackMouse] = useState(false);
-  const [animationComplete, setAnimationComplete] = useState(true);
-  const [items, setItems] = useState([]);
-
+  //FUNCIONES CARRUSEL 
   useEffect(() => {
     const arr = new Array(10).fill(undefined).map((val, idx) => idx);
     setItems(arr);
   }, []);
-
   const x = useMotionValue(0);
 
   const handleMouseMove = (e) => {
     if (!ref.current) return;
     if (!trackMouse) return;
-
     setAnimationComplete(false);
-
     const xVal = e.pageX - ref.current.offsetLeft;
     const walk = (xVal - startX) * 2;
-
     const controls = animate(x, scrollLeft - walk, {
       type: "tween",
       ease: "easeOut",
@@ -79,33 +59,19 @@ const MovieList = () => {
     });
     return controls.stop;
   };
-
   const handleMouseDown = (e) => {
     if (!ref.current) return;
-
     setTrackMouse(true);
-
     const startX = e.pageX - ref.current.offsetLeft;
     setStartX(startX);
-
     const scrollLeft = ref.current.scrollLeft;
     setScrollLeft(scrollLeft);
   };
-
   const handleMouseLeave = () => {
     setTrackMouse(false);
   };
-
   const handleMouseUp = () => {
     setTrackMouse(false);
-  };
-
-  const handleScroll = () => {
-    if (!ref.current) return;
-
-    if (animationComplete) {
-      x.set(ref.current.scrollLeft);
-    }
   };
 
   return (
@@ -118,26 +84,26 @@ const MovieList = () => {
             <h1>Peliculas</h1>
 
             <div className="MovieList">
+
               <motion.ul
                 ref={ref}
                 onMouseMove={handleMouseMove}
                 onMouseDown={(e) => {
-                  e.preventDefault(); // Evitar el comportamiento predeterminado del arrastre
+                  e.preventDefault();
                   handleMouseDown(e);
                 }}
                 onMouseUp={handleMouseUp}
                 onMouseLeave={handleMouseLeave}
-                onScroll={handleScroll}
               >
                 {movies.map((movie) => (
-                  
                   <motion.li key={movie.id} >
-                      <Link to={`/pelicula/id/${movie.id}`}>
-                        <Card imgUrl={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} title={movie.title} />
-                      </Link>
+                    <Link to={`/pelicula/id/${movie.id}`}>
+                      <Card imgUrl={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} title={movie.title} />
+                    </Link>
                   </motion.li>
                 ))}
               </motion.ul>
+
             </div>
           </div>
         )}
