cf9638f3ae42bd893cf2a27f82b3f3815e9008f5
Shyedelta <shirodelta@gmail.com>
2024-02-29 01:34:09 +0100

    Mejorando codigo

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -33,76 +33,75 @@ const Header = () => {
 
     return (
         <>
-            {!isLoginPage && (
-                <Box
-                    bg="black"
-                    fontFamily={"kanit, sans-serif"}
-                    fontWeight={700}
+            <Box
+                bg="black"
+                fontFamily={"kanit, sans-serif"}
+                fontWeight={700}
+            >
+                <Flex
+                    h="3em"
+                    bg="#CC3344"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    overflowY="hidden"
+                    px="1em"
                 >
-                    <Flex
-                        h="3em"
-                        bg="#CC3344"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        overflowY="hidden"
-                        px="1em"
-                    >
-                        <Flex w="4em">
-                            <Link to={`/inicio`}>
-                                <Image src={logo} alt='Logo' w="100%" aspectRatio="4/4" filter="invert(1)" p="0.5em" mx="1em" transition="0.2s" _hover={{ transform: "scale(1.1)" }} />
-                            </Link>
-                        </Flex>
-                        <Flex
-                            listStyleType="none"
-                            transition={{ color: '0.3s', fontSize: '0.3s' }}
-                        >
-                            {links.map((link) => (
-                                <Link key={link.name} to={link.href}>
-                                    <button className='button' px="1em"
-                                        w="maxcontent"
-                                        h="70px"
-                                        display="flex"
-                                        align-items="center"
-                                        justify-content="center"
-                                        fontSize="26"
-                                        transition="0.4s"
-                                    >
-                                        {link.name}
-                                    </button>
-                                </Link>
-                            ))}
-                        </Flex>
-
-                        <Flex
-                            transition="0.4s"
-                            alignItems="center"
-                            justifyContent="center"
-                        >
-                            <Input
-                                onChange={handleChange}
-                                value={busqueda}
-                                placeholder='Busca tu película favorita!'
-                                _placeholder={{ color: 'white' }}
-                                textColor="white"
-                                focusBorderColor='red.800'
-                                variant='flushed'
-                                textTransform="uppercase"
-                                w="16em"
-                            />
-                            <Link to={`/search/${busqueda.trim()}`}>
-                                <Button
-                                    bg="transparent"
-                                    border="none"
-                                    cursor="pointer"
-                                    _hover={{ bg: "transparent", color: "red" }}
-                                >
-                                    <SearchIcon color="white" boxSize="8" />
-                                </Button>
-                            </Link>
-                        </Flex>
+                    <Flex w="4em">
+                        <Link to={`/inicio`}>
+                            <Image src={logo} alt='Logo' w="100%" aspectRatio="4/4" filter="invert(1)" p="0.5em" mx="1em" transition="0.2s" _hover={{ transform: "scale(1.1)" }} />
+                        </Link>
                     </Flex>
-                </Box>
-            )}
+                    <Flex
+                        listStyleType="none"
+                        transition={{ color: '0.3s', fontSize: '0.3s' }}
+                    >
+                        {links.map((link) => (
+                            <Link key={link.name} to={link.href}>
+                                <button className='button' px="1em"
+                                    w="maxcontent"
+                                    h="70px"
+                                    display="flex"
+                                    align-items="center"
+                                    justify-content="center"
+                                    fontSize="26"
+                                    transition="0.4s"
+                                >
+                                    {link.name}
+                                </button>
+                            </Link>
+                        ))}
+                    </Flex>
+
+                    <Flex
+                        transition="0.4s"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Input
+                            onChange={handleChange}
+                            value={busqueda}
+                            placeholder='Busca tu película favorita!'
+                            _placeholder={{ color: 'white' }}
+                            textColor="white"
+                            focusBorderColor='red.800'
+                            variant='flushed'
+                            textTransform="uppercase"
+                            w="16em"
+                        />
+                        <Link to={`/search/${busqueda.trim()}`}>
+                            <Button
+                                bg="transparent"
+                                border="none"
+                                cursor="pointer"
+                                _hover={{ bg: "transparent", color: "red" }}
+                            >
+                                <SearchIcon color="white" boxSize="8" />
+                            </Button>
+                        </Link>
+                    </Flex>
+                </Flex>
+            </Box>
+
         </>
     );
 };
